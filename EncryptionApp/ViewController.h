9d5a79019b84e6eed5e741f2c015a24ce5ed459f
s510607 <darionhiggins@nw42846.nwmissouri.edu>
2018-04-09 16:24:43 -0500

    Initial Commit

---
+++ EncryptionApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptionApp
+//
+//  Created by Darion Higgins on 4/9/18.
+//  Copyright © 2018 Darion Higgins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
